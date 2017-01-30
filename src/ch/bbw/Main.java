b79a2d277376dd83448dc05742070b43ed5a3bfe
5ia14sejacobi <5ia14sejacobi@gibwintra.ch>
2017-01-30 14:38:50 +0100

    ad

---
+++ src/ch/bbw/Main.java
@@ -0,0 +1,10 @@
+package ch.bbw;
+
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("mueter");
+
+	}
+
+}
---
+++ src/ch/bbw/test.java
@@ -0,0 +1,15 @@
+package ch.bbw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		}
+
+
+}
